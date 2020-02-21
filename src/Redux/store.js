a5e2b54b1938deb7adfbfaecbc0794407707de62
DasGMA <dasgrigoma@gmail.com>
2020-02-20 19:33:22 -0800

    Fresh react app

---
+++ src/Redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+//import logger from 'redux-logger';
+import rootReducer from './Reducers/rootReducer';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+export default store;
